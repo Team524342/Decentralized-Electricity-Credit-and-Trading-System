f8c5d3569325bd6f94c3095939e624f286eb1a65
MA-FerozAli <your.email@example.com>
2025-10-25 11:31:14 +0530

    env file is created

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('token/<str:consumer_id>',views.get_token_balance),
     path('buy/<str:consumer_id>',views.buy_tokens),
+    path('energy/<str:consumer_id>',views.get_energy_usage),
 ]
--- dects/src/App.js
+++ dects/src/App.js
@@ -1,13 +1,42 @@
 
-
+import React from 'react';
 import ConsumerDashboard from './pages/ConsumerDashboard';
+import {BrowserRouter as Router ,Routes,Route,Link} from 'react-router-dom';
+import Register from './pages/Register';
+
 
 function App() {
+  
   return (
-   <div>
-    <ConsumerDashboard />
-   </div>
-  );
-}
+    <Router>
+      <nav style={styles.navbar}>
+        <Link to="/" style={styles.link}>Register</Link>
+        <Link to='/consumer' style={styles.link}>Consumer Dashboard</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Register/>} />
+        <Route path='/consumer' element={<ConsumerDashboard />} />
+      </Routes>
 
+
+    </Router>
+   
+   
+  
+  );
+  
+}
+const styles ={
+    navbar:{
+    backgroundColor: '#007bff',
+    padding: '10px',
+    textAlign: 'center',
+    },
+    link:{
+    color: 'white',
+    margin: ' 0 10px',
+    textDecoration: 'none',
+    fontWeight:'bold',
+    },
+  };
 export default App;
